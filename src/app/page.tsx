4c593589175d4f9d23b12350cc6123daab61ea48
IgoorToorres <igorevangelista121@gmail.com>
2026-02-10 16:36:23 -0300

    revalidando conteudo a cada 60 segundos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { LinksPage } from "@/templates/links-page";
 import { Metadata } from "next";
 
+export const revalidate = 60;
+
 export const metadata: Metadata = {
   title: 'NextLinks Desafio RocketSeat',
   description: 'Site do desafio da rocketSeat feito por Igor Torres do curso de Next-js',
