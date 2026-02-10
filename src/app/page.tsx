6b5459822a9e453e0e054d8019977813433c0a29
IgoorToorres <igorevangelista121@gmail.com>
2026-02-10 16:29:18 -0300

    feat - melhorando SEO com link personalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,28 @@
 import { LinksPage } from "@/templates/links-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'NextLinks',
+  description: 'Site do desafio da rocketSeat feito por Igor Torres',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'NextLinks',
+    description: 'Site do desafio da rocketSeat feito por Igor Torres',
+    url: 'https://dev-link-two-sandy.vercel.app/',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://dev-link-two-sandy.vercel.app/thumbnail.png',
+        width: 800,
+        height: 600,
+        alt: 'NextLinks'
+      }
+    ]
+  }
+}
+
 
 export default function Home() {
   return (<LinksPage />)
